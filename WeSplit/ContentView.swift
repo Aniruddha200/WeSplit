7d6c785b6660670c222b47affbc4514561fd1454
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-03 09:18:14 +0100

    Alpha Commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount = ""
+    @State private var totalPeople = 2
+    @State private var tipsAmount = 10
+    
+    func splitCalculator() -> Double{
+        let amountBT = Double(billAmount) ?? 0.0
+		let tAmount = Double(tipsAmount)
+        let amountAT = amountBT * (1+(tAmount/100))
+        let cpp = amountAT / Double(totalPeople + 1)
+        return cpp
+    }
+    
+    var tips = [0, 10, 20]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter the Bill Amount", text: $billAmount)
+                        .keyboardType(.decimalPad)
+                }
+                    
+                    Section(header: Text("People & Tip")){
+                        Picker("Select number of people", selection: $totalPeople){
+                            ForEach(1..<100){
+                                Text("\($0)")
+                            }
+                        }
+                        Section(header: Text("Tip Amount")){
+                            Picker("Select tip",selection: $tipsAmount){
+                                ForEach(0..<tips.count){
+                                    Text("\(tips[$0])")
+                                }
+                                
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                    }
+                Section(header: Text("Per Person")){
+                    Text(String(format: "%.2f", splitCalculator()))
+						.foregroundColor(self.tipsAmount > 0 ? Color.green : Color.red)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                }
+                
+            }
+        }
     }
 }
 
